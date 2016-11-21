0307d1fda38946b0116c82ba6c04ed00ba497295
Patrick Mylund Nielsen <patrick@patrickmn.com>
2016-11-21 11:51:55 -0500

    Pin Vagrant quorum clone to specific release (q1.0.0)

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -26,6 +26,7 @@ echo 'PATH=$PATH:/usr/local/go/bin' >> /home/ubuntu/.bashrc
 # make/install quorum
 git clone https://github.com/jpmorganchase/quorum.git
 pushd quorum >/dev/null
+git checkout tags/q1.0.0
 make all
 cp build/bin/geth /usr/local/bin
 cp build/bin/bootnode /usr/local/bin
