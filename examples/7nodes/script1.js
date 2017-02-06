98a83b3ee78d31e3e15ef49d7a20cee209590350
jpmsam <samer.falah@jpmchase.com>
2017-02-05 21:20:36 -0500

    updated script to access solidity compiler output by index

--- examples/7nodes/script1.js
+++ examples/7nodes/script1.js
@@ -2,9 +2,10 @@ a = eth.accounts[0]
 web3.eth.defaultAccount = a;
 
 var simpleSource = 'contract simplestorage { uint public storedData; function simplestorage(uint initVal) { storedData = initVal; } function set(uint x) { storedData = x; } function get() constant returns (uint retVal) { return storedData; } }'
-var simpleCompiled = web3.eth.compile.solidity(simpleSource)
-var simpleContract = web3.eth.contract(simpleCompiled.simplestorage.info.abiDefinition);
-var simple = simpleContract.new(42, {from:web3.eth.accounts[0], data: simpleCompiled.simplestorage.code, gas: 300000, privateFor: ["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]}, function(e, contract) {
+var simpleCompiled = web3.eth.compile.solidity(simpleSource);
+var simpleRoot = Object.keys(simpleCompiled)[0];
+var simpleContract = web3.eth.contract(simpleCompiled[simpleRoot].info.abiDefinition);
+var simple = simpleContract.new(42, {from:web3.eth.accounts[0], data: simpleCompiled[simpleRoot].code, gas: 300000, privateFor: ["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]}, function(e, contract) {
   if (e) {
     console.log("err creating contract", e);
   } else {
