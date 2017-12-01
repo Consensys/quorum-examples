c50e97a362e6335b67852f44b72cbd25c9a28d5c
Joel Burget <joelburget@gmail.com>
2017-12-01 10:14:56 -0500

    Copy permissioned-nodes.json into datadirs.

--- examples/7nodes/raft-init.sh
+++ examples/7nodes/raft-init.sh
@@ -6,30 +6,34 @@ echo "[*] Cleaning up temporary data directories"
 rm -rf qdata
 mkdir -p qdata/logs
 
-echo "[*] Configuring node 1"
+echo "[*] Configuring node 1 (permissioned)"
 mkdir -p qdata/dd1/{keystore,geth}
 cp permissioned-nodes.json qdata/dd1/static-nodes.json
+cp permissioned-nodes.json qdata/dd1/
 cp keys/key1 qdata/dd1/keystore
 cp raft/nodekey1 qdata/dd1/geth/nodekey
 geth --datadir qdata/dd1 init genesis.json
 
-echo "[*] Configuring node 2"
+echo "[*] Configuring node 2 (permissioned)"
 mkdir -p qdata/dd2/{keystore,geth}
 cp permissioned-nodes.json qdata/dd2/static-nodes.json
+cp permissioned-nodes.json qdata/dd2/
 cp keys/key2 qdata/dd2/keystore
 cp keys/key3 qdata/dd2/keystore
 cp raft/nodekey2 qdata/dd2/geth/nodekey
 geth --datadir qdata/dd2 init genesis.json
 
-echo "[*] Configuring node 3"
+echo "[*] Configuring node 3 (permissioned)"
 mkdir -p qdata/dd3/{keystore,geth}
 cp permissioned-nodes.json qdata/dd3/static-nodes.json
+cp permissioned-nodes.json qdata/dd3/
 cp raft/nodekey3 qdata/dd3/geth/nodekey
 geth --datadir qdata/dd3 init genesis.json
 
-echo "[*] Configuring node 4"
+echo "[*] Configuring node 4 (permissioned)"
 mkdir -p qdata/dd4/{keystore,geth}
 cp permissioned-nodes.json qdata/dd4/static-nodes.json
+cp permissioned-nodes.json qdata/dd4/
 cp keys/key4 qdata/dd4/keystore
 cp raft/nodekey4 qdata/dd4/geth/nodekey
 geth --datadir qdata/dd4 init genesis.json
--- examples/7nodes/raft-start.sh
+++ examples/7nodes/raft-start.sh
@@ -2,7 +2,7 @@
 set -u
 set -e
 
-GLOBAL_ARGS="--verbosity 5 --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
+GLOBAL_ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
 
 echo "[*] Starting Constellation nodes"
 nohup constellation-node tm1.conf 2>> qdata/logs/constellation1.log &
