29d14b43f96d9a9f213f7c07a931a90d4b17fb01
drandreaskrueger <andreas.krueger@electron.org.uk>
2018-04-10 12:23:29 +0100

    fixed copy paste error

--- examples/7nodes/init-private.js
+++ examples/7nodes/init-private.js
@@ -16,9 +16,9 @@ else {
         console.log("var BLOCKNUMBER was not set = using newest sealed block " + block);
 }
 
-var address=web3.eth.getTransactionReceipt(web3.eth.getBlock(block).transactions[0])["contractAddress"]; // assuming transaction[0] can actually fail if there are other transactions $
-var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type"$
-var private = eth.contract(abi).at(address);
+var address=web3.eth.getTransactionReceipt(web3.eth.getBlock(block).transactions[0])["contractAddress"]; // assuming transaction 0 - can fail if there are other transactions in same block
+var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"type":"constructor"}];
+var private = eth.contract(abi).at(address)
 
 console.log("Done. Initialized 'private' with deployed contract. You can use private.get() and private.set() now.")
 
