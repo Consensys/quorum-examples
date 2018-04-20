886c1f8c5cfa13d694d5955e1c64a68cf6e7f67c
jpmsam <samer.falah@jpmchase.com>
2018-04-20 10:10:06 -0400

    added nodiscover flag

--- examples/7nodes/istanbul-start.sh
+++ examples/7nodes/istanbul-start.sh
@@ -4,11 +4,11 @@ set -e
 
 mkdir -p qdata/logs
 echo "[*] Starting Constellation nodes"
-./constellation-start.sh
+#./constellation-start.sh
 
 echo "[*] Starting Ethereum nodes"
 set -v
-ARGS="--syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
+ARGS="--nodiscover --syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
 PRIVATE_CONFIG=qdata/c1/tm.ipc nohup geth --datadir qdata/dd1 $ARGS --rpcport 22000 --port 21000 --unlock 0 --password passwords.txt 2>>qdata/logs/1.log &
 PRIVATE_CONFIG=qdata/c2/tm.ipc nohup geth --datadir qdata/dd2 $ARGS --rpcport 22001 --port 21001 2>>qdata/logs/2.log &
 PRIVATE_CONFIG=qdata/c3/tm.ipc nohup geth --datadir qdata/dd3 $ARGS --rpcport 22002 --port 21002 2>>qdata/logs/3.log &
@@ -20,4 +20,4 @@ set +v
 
 echo
 echo "All nodes configured. See 'qdata/logs' for logs, and run e.g. 'geth attach qdata/dd1/geth.ipc' to attach to the first Geth node."
-echo "To test sending a private transaction from Node 1 to Node 7, run './runscript script1.js'"
+echo "To test sending a private transaction from Node 1 to Node 7, run './runscript.sh script1.js'"
