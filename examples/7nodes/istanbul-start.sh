2ae487d7fba74b7489f5981b14e9d6b49e2afe4b
jpmsam <samer.falah@jpmchase.com>
2018-05-23 11:22:36 -0400

    modified private contract script name

--- examples/7nodes/istanbul-start.sh
+++ examples/7nodes/istanbul-start.sh
@@ -20,4 +20,4 @@ set +v
 
 echo
 echo "All nodes configured. See 'qdata/logs' for logs, and run e.g. 'geth attach qdata/dd1/geth.ipc' to attach to the first Geth node."
-echo "To test sending a private transaction from Node 1 to Node 7, run './runscript.sh script1.js'"
+echo "To test sending a private transaction from Node 1 to Node 7, run './runscript.sh private-contract.js'"
