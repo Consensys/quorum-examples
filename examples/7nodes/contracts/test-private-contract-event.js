0a1961962dbeb7672ac20fc1c0e51aec7f00016e
Libby Kent <viskovitzzz@gmail.com>
2018-07-27 13:47:08 -0400

    Adding event tests which will submit transaction to  contracts.
    Hardcoded to submit 20 events against a specified contract addr.

---
+++ examples/7nodes/contracts/test-private-contract-event.js
@@ -0,0 +1,13 @@
+// From geth console run
+//   $> loadScript('event_test_priv.js')
+// From console test node 1 & 7 to see if the events were recorded
+//   $> seth events 0x1932c48b2bf8102ba33b4a6b545c32236e342f34 | wc -l
+var address="0x1932c48b2bf8102ba33b4a6b545c32236e342f34"
+
+// simple contract
+var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
+var contract = eth.contract(abi).at(address)
+
+for ( i = 0; i < 20; i++) {
+  contract.set(4,{from:eth.accounts[0], privateFor:["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]});
+} 
