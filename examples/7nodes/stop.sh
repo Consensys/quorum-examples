45afc0c7449c087c9fff0106adac5ff9ad0af849
namtruong <nam.p.truong@outlook.com>
2018-08-15 20:15:13 +0100

    correct stop script to search for tessera process

--- examples/7nodes/stop.sh
+++ examples/7nodes/stop.sh
@@ -3,7 +3,7 @@ killall geth bootnode constellation-node
 
 if [ "`jps | grep tessera`" != "" ]
 then
-  jps | grep tessera-app | cut -d " " -f1 | xargs kill
+  jps | grep tessera | cut -d " " -f1 | xargs kill
 else
   echo "tessera-app: no process found"
 fi
