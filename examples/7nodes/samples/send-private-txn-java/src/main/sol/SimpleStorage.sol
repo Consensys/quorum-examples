ef26150581a2b13d6963b4da525fcbd04e5623bf
nicolae <nicolae.leonte.go@gmail.com>
2019-05-03 18:41:14 +0100

    Add js and java examples

---
+++ examples/7nodes/samples/send-private-txn-java/src/main/sol/SimpleStorage.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract SimpleStorage {
+    uint private storedData;
+
+    constructor(uint initVal) public {
+        storedData = initVal;
+    }
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint retVal) {
+        return storedData;
+    }
+}
